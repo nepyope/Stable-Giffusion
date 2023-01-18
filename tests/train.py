b3dd3cfaace4c8f9b3021b2101c1d9e60eb1e54e
ClashLuke <39779310+ClashLuke@users.noreply.github.com>
2023-01-18 20:15:03 +0100

    fix(train): each only has a slice

--- tests/train.py
+++ tests/train.py
@@ -144,7 +144,7 @@ def main(lr: float = 1e-4, beta1: float = 0.9, beta2: float = 0.99, weight_decay
 
             # TODO: use perceptual loss
             dist = out - batch["pixel_values"]
-            dist = dist.reshape(batch_size, context, *dist.shape[1:])
+            dist = dist.reshape(-1, context, *dist.shape[1:])
             dist_sq = lax.pmean(lax.square(dist).mean((0, 2, 3, 4)), "batch")
             dist_abs = lax.pmean(dist.abs().mean((0, 2, 3, 4)), "batch")
             return dist_sq.mean(), (dist_sq, dist_abs)
