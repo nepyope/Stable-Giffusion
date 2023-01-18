7c6f9db6a510ba352514a0e268e604c76de3a0da
ClashLuke <39779310+ClashLuke@users.noreply.github.com>
2023-01-18 14:50:41 +0100

    feat: add install script

---
+++ install.sh
@@ -0,0 +1,5 @@
+python3 -m pip install --upgrade pip
+python3 -m pip install --no-cache-dir --force-reinstall --upgrade "jax[tpu]" -f https://storage.googleapis.com/jax-releases/libtpu_releases.html
+sudo python3 -m pip uninstall tensorboard tbp-nightly tb-nightly tensorboard-plugin-profile -y
+python3 -m pip install wandb smart-open[gcs] jsonpickle sharedutils git+https://github.com/ytdl-org/youtube-dl/ typer diffusers flax optax
+python3 -m pip install --upgrade --force-reinstall tensorflow==2.8.0 protobuf==3.20.1
