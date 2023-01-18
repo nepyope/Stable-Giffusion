150a708b117b07488fae10ff2096ed9e599658db
ClashLuke <39779310+ClashLuke@users.noreply.github.com>
2023-01-18 12:34:32 +0100

    feat(train): first train vae; use production YT pipeline

---
+++ tests/data.py
@@ -0,0 +1,194 @@
+import dataclasses
+import json
+import multiprocessing
+import multiprocessing.shared_memory
+import os
+import random
+import shutil
+import threading
+import traceback
+import uuid
+from datetime import datetime
+from queue import Empty
+from typing import List, Callable
+
+import ffmpeg
+import numpy as np
+import requests
+import youtube_dl
+
+_DONE = "DONE"
+
+
+@dataclasses.dataclass
+class Share:
+    dtype: np.dtype
+    shape: List[int]
+    name: str
+
+
+def to_share(inp: np.array, smm: multiprocessing.managers.SharedMemoryManager) -> Share:
+    mem = smm.SharedMemory(inp.nbytes)
+    np_mem = np.array(inp.shape, dtype=inp.dtype, buffer=mem.buf)
+    np_mem[:] = inp[:]
+    return Share(dtype=inp.dtype, shape=inp.shape, name=mem.name)
+
+
+def from_share(share: Share) -> np.ndarray:
+    mem = multiprocessing.shared_memory.SharedMemory(name=share.name, create=False)
+    arr = np.copy(np.array(share.shape, share.dtype, buffer=mem.buf))
+    mem.unlink()
+    return arr
+
+
+def try_except(fn: Callable, default=None):
+    def _fn(*args, **kwargs):
+        try:
+            return fn(*args, **kwargs)
+        except Exception as exc:  # skipcq: PYL-W0703
+            print(r"IGNORED EXCEPTION \/\/\/")
+            print(fn, exc)
+            traceback.print_exc()
+            print("IGNORED EXCEPTION /\\/\\/\\")
+
+        return default
+
+    return _fn
+
+
+@try_except
+def get_video_urls(youtube_getter, youtube_base: str, url: str, lock: threading.Semaphore, target_image_size: int) -> \
+        List[dict]:
+    # We have to lock this part because it can lead to errors if multiple thread try to scrape video Information at
+    # the same time.
+    with lock:
+        info = youtube_getter.extract_info(youtube_base + url, download=False)
+    if info is None or 'formats' not in info:
+        return []
+    video_urls = []
+    for f in info['formats']:
+        width = f.get('width')
+        height = f.get('height')
+        url = f.get('url')
+        ext = f.get('ext')
+        format_note = f.get('format_note')
+
+        if any(x is None for x in (width, height, url, ext, format_note)):
+            continue
+        if any(not x for x in (width, height, url, ext)):
+            continue
+        if format_note == "tiny" or width <= target_image_size or height <= target_image_size:
+            continue
+        video_urls.append({'width': width, 'height': height, 'ext': f['ext'], 'url': f['url']})
+    return sorted(video_urls, key=lambda x: (x['ext'] != 'mp4', x['width'], x['height']))
+
+
+def get_video_frames(video_urls: List[dict], target_image_size: int, target_fps: int) -> np.ndarray:
+    filename = uuid.uuid4()
+    path = str(filename)
+    for video_url in video_urls:
+        if os.path.exists(path):
+            os.remove(path)
+
+        url = video_url["url"]
+        path = f"{filename}.{video_url['ext']}"
+
+        try:
+            with requests.get(url, stream=True) as r, open(path, 'wb') as f:
+                shutil.copyfileobj(r.raw, f)
+        except Exception:  # skipcq: PYL-W0703
+            continue  # Broken URL, next might work
+
+        width = round(video_url["width"] * video_url["height"] / target_image_size)
+        try:
+            out, _ = ffmpeg.input(path) \
+                .filter("scale", w=width, h=target_image_size) \
+                .filter("crop", w=target_image_size, h=target_image_size).filter("fps", target_fps) \
+                .output("pipe:", format="rawvideo", pix_fmt="rgb24", loglevel="error", preset="ultrafast",
+                        threads=target_image_size // 40) \
+                .run(capture_stdout=True)
+        except ffmpeg.Error:  # Broken Video, next might work
+            continue
+
+        if os.path.exists(path):
+            os.remove(path)
+        return np.frombuffer(out, np.uint8).reshape((-1, target_image_size, target_image_size, 3))
+
+
+def frame_worker(work: list, worker_id: int, lock: threading.Semaphore, target_image_size: int, target_fps: int,
+                 context_size: int, queue: multiprocessing.Queue):
+    youtube_base = 'https://www.youtube.com/watch?v='
+    youtube_getter = youtube_dl.YoutubeDL(
+        {'writeautomaticsub': False, 'socket_timeout': 600, "quiet": True, "verbose": False, "no_warnings": True,
+         "ignoreerrors": True
+         })
+    youtube_getter.add_default_info_extractors()
+    random.Random(worker_id).shuffle(work)
+
+    with multiprocessing.managers.SharedMemoryManager() as smm:
+        for wor in work:
+            video_urls = get_video_urls(youtube_getter, youtube_base, wor, lock, target_image_size)
+
+            if not video_urls:
+                continue
+
+            frames = get_video_frames(video_urls, target_image_size, target_fps)
+
+            if frames is None or not frames.size or frames.shape[0] < context_size:
+                continue
+            frames = frames[:frames.shape[0] // context_size * context_size]
+            frames = frames.reshape(-1, context_size, *frames.shape[1:])
+            for ctx in frames:
+                queue.put(to_share(ctx, smm))
+    queue.put(_DONE)
+
+
+class DataLoader:
+    def __init__(self, workers: int, url_dir: str, video_downloaders: int, resolution: int, fps: int, context: int,
+                 batch_size: int, prefetch: int, seed: int = 0):
+        self.workers = workers
+        self.video_downloaders = video_downloaders
+        self.resolution = resolution
+        self.fps = fps
+        self.context = context
+        self.prefetch = prefetch
+        self.batch_size = batch_size
+        self.seed = seed
+        self.ids = ids = []
+        for path in os.listdir(url_dir):
+            with open(f'{url_dir}/{path}', 'rb') as f:
+                vals = json.load(f)
+                ids.extend([x for i, d in zip(vals["id"], vals["duration"]) for x, z in zip(i, d) if z > context / fps])
+
+    def __iter__(self):
+        random.Random(self.seed).shuffle(self.ids)
+        lock = multiprocessing.Semaphore(self.video_downloaders)
+        queue = multiprocessing.Queue(self.prefetch)
+        workers = []
+        for i in range(self.workers):
+            work = self.ids[int(len(self.ids) * i / self.workers):int(len(self.ids) * (i + 1) / self.workers)]
+            workers.append(multiprocessing.Process(args=(work, i, lock, self.resolution, self.fps, self.context, queue),
+                                                   daemon=True, target=frame_worker))
+        for w in workers:
+            w.start()
+
+        done = 0
+        batch = []
+        while True:
+            if done == self.workers:
+                break
+            try:
+                out = queue.get(timeout=120)
+            except Empty:
+                print(datetime.datetime.now(), "Queue empty. Waiting another 120 seconds.")
+                continue
+            if out == _DONE:
+                done += 1
+                continue
+            batch.append(from_share(out))
+            if len(batch) == self.batch_size:
+                yield np.stack(batch)
+                batch.clear()
+        for w in workers:
+            w.join()
+        raise StopIteration
