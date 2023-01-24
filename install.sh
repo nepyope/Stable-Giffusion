77c77862e0c41cf8c4efedd303deaaa6249f6a10
nepyope <nopyeps@gmail.com>
2023-01-24 23:03:57 +0000

    fix install

--- install.sh
+++ install.sh
@@ -1,12 +1,10 @@
 python3 -m pip install --upgrade pip
 python3 -m pip install --no-cache-dir --force-reinstall --upgrade "jax[tpu]" -f https://storage.googleapis.com/jax-releases/libtpu_releases.html
 sudo python3 -m pip uninstall tensorboard tbp-nightly tb-nightly tensorboard-plugin-profile -y
-python3 -m pip install wandb smart-open[gcs] jsonpickle sharedutils git+https://github.com/ytdl-org/youtube-dl/ typer diffusers flax optax ffmpeg-python huggingface-hub transformers
+python3 -m pip install wandb smart-open[gcs] jsonpickle sharedutils git+https://github.com/ytdl-org/youtube-dl/ typer diffusers flax optax ffmpeg-python huggingface-hub transformers gdown torch torchvision opencv-python 
 python3 -m pip install --upgrade --force-reinstall tensorflow==2.8.0 protobuf==3.20.1
-sudo apt remove python3-pip
 wget https://files.pythonhosted.org/packages/a3/50/c4d2727b99052780aad92c7297465af5fe6eec2dbae490aa9763273ffdc1/pip-22.3.1.tar.gz
 tar -xzvf pip-22.3.1.tar.gz
 cd pip-22.3.1
 python3 -m setup.py install
 cd .. 
-rm -r pip-22.3.1
