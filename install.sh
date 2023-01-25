b9d9bab803e07a9e18770d346464e4d9e023bfd4
ClashLuke <39779310+ClashLuke@users.noreply.github.com>
2023-01-25 13:00:00 +0100

    fix(install): add ftfy

--- install.sh
+++ install.sh
@@ -1,8 +1,9 @@
 python3 -m pip install --upgrade pip
 python3 -m pip install --no-cache-dir --force-reinstall --upgrade "jax[tpu]" -f https://storage.googleapis.com/jax-releases/libtpu_releases.html
 sudo python3 -m pip uninstall tensorboard tbp-nightly tb-nightly tensorboard-plugin-profile -y
-python3 -m pip install wandb smart-open[gcs] jsonpickle sharedutils git+https://github.com/ytdl-org/youtube-dl/ typer diffusers flax optax ffmpeg-python huggingface-hub transformers gdown torch torchvision opencv-python 
+python3 -m pip install wandb smart-open[gcs] jsonpickle sharedutils git+https://github.com/ytdl-org/youtube-dl/ typer diffusers flax optax ffmpeg-python huggingface-hub transformers gdown torch torchvision opencv-python ftfy
 python3 -m pip install --upgrade --force-reinstall tensorflow==2.8.0 protobuf==3.20.1
+
 wget https://files.pythonhosted.org/packages/a3/50/c4d2727b99052780aad92c7297465af5fe6eec2dbae490aa9763273ffdc1/pip-22.3.1.tar.gz
 tar -xzvf pip-22.3.1.tar.gz
 cd pip-22.3.1
