a34c659f96d251c7f16fdb2ba2e01c039400d14a
Martino Russi <77496684+nepyope@users.noreply.github.com>
2023-02-15 20:22:09 +0100

    increase num workers

--- tests/train.py
+++ tests/train.py
@@ -178,7 +178,7 @@ def load(path: str, prototype: Dict[str, jax.Array]):
 
 @app.command()
 def main(lr: float = 2e-5, beta1: float = 0.9, beta2: float = 0.99, eps: float = 1e-16, downloaders: int = 2,
-         resolution: int = 128, fps: int = 1, context: int = 8, workers: int = 16, prefetch: int = 8,
+         resolution: int = 128, fps: int = 1, context: int = 8, workers: int = 64, prefetch: int = 8,
          batch_prefetch: int = 2, base_model: str = "flax/stable-diffusion-2-1", data_path: str = "./urls",
          sample_interval: int = 2048, parallel_videos: int = 2048, schedule_length: int = 1024, warmup_steps: int = 1024,
          lr_halving_every_n_steps: int = 2 ** 17, clip_tokens: int = 77, save_interval: int = 2048,
