5dc8aa2ca2dc12754a8c842c40a88cfce93e1919
Martino Russi <77496684+nepyope@users.noreply.github.com>
2023-02-16 22:51:14 +0100

    yt_dlp

--- install.sh
+++ install.sh
@@ -4,3 +4,4 @@ sudo python3 -m pip uninstall tensorboard tbp-nightly tb-nightly tensorboard-plu
 python3 -m pip install wandb smart-open[gcs] jsonpickle sharedutils git+https://github.com/ytdl-org/youtube-dl/ typer diffusers flax optax ffmpeg-python huggingface-hub transformers gdown torch torchvision opencv-python ftfy
 python3 -m pip install --upgrade --force-reinstall tensorflow==2.8.0 protobuf==3.20.1
 python3 -m pip install --no-cache-dir --force-reinstall --upgrade "jax[tpu]==0.4.1" -f https://storage.googleapis.com/jax-releases/libtpu_releases.html
+python3 -m pip install --force-reinstall https://github.com/yt-dlp/yt-dlp/archive/master.tar.gz
