a53e5648d32386203b4a2ca0331ad7758bad79b3
nepyope <nopyeps@gmail.com>
2023-02-17 20:18:24 +0100

    base path

--- tests/train.py
+++ tests/train.py
@@ -175,17 +175,16 @@ def load(path: str, prototype: Dict[str, jax.Array]):
     _, tree = jax.tree_util.tree_flatten(prototype)
     return tree.unflatten(params)
 
-
 @app.command()
 def main(lr: float = 2e-5, beta1: float = 0.9, beta2: float = 0.99, eps: float = 1e-16, downloaders: int = 2,
          resolution: int = 128, fps: int = 1, context: int = 8, workers: int = 8, prefetch: int = 8,
-         batch_prefetch: int = 2, base_model: str = "flax/stable-diffusion-2-1", data_path: str = "./urls",
+         batch_prefetch: int = 2, base_model: str = "gs://video-us/flax_base_model/", data_path: str = "./urls",
          sample_interval: int = 2048, parallel_videos: int = 128, schedule_length: int = 1024, warmup_steps: int = 1024,
          lr_halving_every_n_steps: int = 2 ** 17, clip_tokens: int = 77, save_interval: int = 2048,
          overwrite: bool = True, base_path: str = "gs://video-us/checkpoint/", local_iterations: int = 4,
          unet_batch: int = 1, device_steps: int = 4):
     
-    tokenizer = CLIPTokenizer.from_pretrained(base_model, subfolder="tokenizer",use_auth_token=True)
+    tokenizer = CLIPTokenizer.from_pretrained(base_model, subfolder="tokenizer")
     data = DataLoader(workers, data_path, downloaders, resolution, fps, context, jax.local_device_count(), prefetch,
                       parallel_videos, tokenizer, clip_tokens, device_steps, batch_prefetch)
 
