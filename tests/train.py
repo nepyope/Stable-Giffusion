61c07deea3dbc6654fca06c78151772eee6bb1f5
nepyope <nopyeps@gmail.com>
2023-02-19 04:05:54 +0100

    comapre to old dataloader

--- tests/train.py
+++ tests/train.py
@@ -25,7 +25,7 @@ from optax._src.numerics import safe_int32_increment
 from optax._src.transform import ScaleByAdamState
 from transformers import CLIPTokenizer, FlaxCLIPTextModel
 
-from data import DataLoader
+from old_data import DataLoader
 
 app = typer.Typer(pretty_exceptions_enable=False)
 check_min_version("0.10.0.dev0")
@@ -156,6 +156,31 @@ def scale_by_laprop(b1: float, b2: float, eps: float, lr: optax.Schedule, clip: 
 
     return GradientTransformation(init_fn, update_fn)
 
+
+def scale_by_laprop(b1: float, b2: float, eps: float, lr: optax.Schedule, clip: float = 1e-2) -> GradientTransformation:
+    def init_fn(params):
+        return {"momentum": jax.tree_util.tree_map(jnp.zeros_like, params), "count": jnp.zeros((), dtype=jnp.int64)}
+
+    def update_fn(updates, state, params=None):
+        count = state["count"] + 1
+
+        def get_update(grad: jax.Array, param: jax.Array, mom: jax.Array):
+            dtype = mom.dtype
+            grad, param, mom = jax.tree_map(promote, (grad, param, mom))
+            g_norm = clip_norm(grad, 1e-16)
+            p_norm = clip_norm(param, 1e-3)
+            grad *= lax.min(p_norm / g_norm * clip, 1.)
+
+            delta = grad - mom
+            update = mom + delta * (1 - b1)
+            return update * -lr(count), (mom + delta * (1 - b2)).astype(dtype)
+
+        leaves, treedef = jax.tree_util.tree_flatten(updates)
+        all_leaves = [leaves] + [treedef.flatten_up_to(r) for r in (params, state.nu, state.mu)]
+        updates, mom = [treedef.unflatten(leaf) for leaf in zip(*[get_update(*xs) for xs in zip(*all_leaves)])]
+        return jnp.sign(updates), {"momentum": mom, "count": count}
+
+    return GradientTransformation(init_fn, update_fn)
 
 def deep_replace(d, value):
     if isinstance(d, dict):
