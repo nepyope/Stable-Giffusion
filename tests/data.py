35f7a87f5e79778d387979df3f95f651b8817942
Martino Russi <77496684+nepyope@users.noreply.github.com>
2023-02-20 21:28:54 +0100

    tokenization shape

--- tests/data.py
+++ tests/data.py
@@ -303,16 +303,17 @@ class DataLoader:
                 if _DEBUG:
                     self.batch_queue.put([hashlib.sha3_512(s.encode()).hexdigest() for s in subs])
                     continue
-                print('subs ',len(subs), subs[0].shape)
-                print('pixels ',len(np_batch), np_batch[0].shape)
+
                 input_ids = []
                 attention_mask = []
 
                 for sub in subs:
                     tokens = self.tokenizer(sub.tolist(), return_tensors="np", padding="max_length", truncation=True,
                                             max_length=self.clip_tokens)
-                    input_ids.append(tokens["input_ids"].reshape(self.batch_size, -1))
-                    attention_mask.append(tokens["attention_mask"].reshape(self.batch_size, -1))
+                    input_ids.append(tokens["input_ids"])
+                    attention_mask.append(tokens["attention_mask"])
+                    print(input_ids[0].shape, attention_mask[0].shape)
+                    print('after tokenization: ',len(input_ids), len(attention_mask))
                 self.batch_queue.put((np.stack(np_batch), np.stack(input_ids), np.stack(attention_mask)))
                 np_batch.clear()
                 subs.clear()
