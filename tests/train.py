7f556acb2adcdd6e84af7ed08a85ee0addeb6e9b
Martino Russi <77496684+nepyope@users.noreply.github.com>
2023-02-20 22:18:31 +0100

    it was not

--- tests/train.py
+++ tests/train.py
@@ -371,9 +371,7 @@ def main(lr: float = 2e-5, beta1: float = 0.9, beta2: float = 0.99, eps: float =
             
             print(f'vid shape AFTER{batch["pixel_values"].shape}')         
             print(batch['idx'].shape)
-            batch = jax.pmap(lambda a: a, "batch")(batch)
-            print(f'vid shape pmap batch{batch["pixel_values"].shape}')    
-            batch = lax.all_to_all(batch, "batch", split_axis=0, concat_axis=1)
+            batch = jax.pmap(lambda x: lax.all_to_all(x, axis_name='i', split_axis=0, concat_axis=1, tiled=True), axis_name='i')(batch)
 
             print(f'vid shape all_to_all{batch["pixel_values"].shape}')    
             
