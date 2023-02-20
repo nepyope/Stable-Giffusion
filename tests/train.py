1506ba4434d796a7c99cd5583923aedd7f85558f
Martino Russi <77496684+nepyope@users.noreply.github.com>
2023-02-21 00:51:44 +0100

    shuffle as global and change to_img

--- tests/train.py
+++ tests/train.py
@@ -30,7 +30,6 @@ from data import DataLoader
 app = typer.Typer(pretty_exceptions_enable=False)
 check_min_version("0.10.0.dev0")
 _UPLOAD_RETRIES = 8
-_SHUFFLE = False
 
 def attention(query: jax.Array, key: jax.Array, value: jax.Array, scale: float):
     ctx_dims = f'{"b" * (key.ndim > 3)}zhf'
@@ -210,7 +209,7 @@ def main(lr: float = 2e-5, beta1: float = 0.9, beta2: float = 0.99, eps: float =
     tokenizer = CLIPTokenizer.from_pretrained(base_model, subfolder="tokenizer")
     data = DataLoader(workers, data_path, downloaders, resolution, fps, context, device_steps, prefetch,
                       parallel_videos, tokenizer, clip_tokens, jax.device_count(), batch_prefetch)
-    
+    global _SHUFFLE 
     vae, vae_params = FlaxAutoencoderKL.from_pretrained(base_model, subfolder="vae", dtype=jnp.float32)
     unet, unet_params = FlaxUNet2DConditionModel.from_pretrained(base_model, subfolder="unet", dtype=jnp.float32)
 
@@ -238,6 +237,7 @@ def main(lr: float = 2e-5, beta1: float = 0.9, beta2: float = 0.99, eps: float =
         return text_encoder(input_ids, attention_mask, params=text_encoder.params)[0]
 
     def unet_fn(noise, encoded, timesteps, params):
+        global _SHUFFLE
         _SHUFFLE = True
         out = unet.apply({"params": params}, lax.stop_gradient(noise), timesteps, lax.stop_gradient(encoded)).sample
         _SHUFFLE = False
@@ -265,8 +265,6 @@ def main(lr: float = 2e-5, beta1: float = 0.9, beta2: float = 0.99, eps: float =
         batch = all_to_all_batch(batch)
         batch = jax.tree_map(lambda x: x[0], batch)
         latent_rng, sample_rng, noise_rng, step_rng = jax.random.split(rng(batch["idx"]), 4)
-        print('ids ', batch["input_ids"].shape)
-        print('idx ', batch["idx"].shape, batch["idx"])
         inp = jnp.transpose(batch["pixel_values"][0].astype(jnp.float32) / 255, (0, 3, 1, 2))
         posterior = vae_apply(inp, method=vae.encode)
 
@@ -311,7 +309,6 @@ def main(lr: float = 2e-5, beta1: float = 0.9, beta2: float = 0.99, eps: float =
     def train_step(unet_state: TrainState, batch: Dict[str, jax.Array]):
         img = batch["pixel_values"].astype(jnp.float32) / 255 
         inp = jnp.transpose(img[0], (0, 3, 1, 2))
-        print(batch["input_ids"].shape)
         gauss0, drop0 = jax.random.split(rng(batch["idx"] + 1), 2)
         vae_out = vae_apply(inp, rngs={"gaussian": gauss0, "dropout": drop0}, deterministic=False, method=vae.encode)
         encoded = get_encoded(batch["input_ids"], batch["attention_mask"])
@@ -359,7 +356,7 @@ def main(lr: float = 2e-5, beta1: float = 0.9, beta2: float = 0.99, eps: float =
     start_time = time.time()
 
     def to_img(x: jax.Array) -> wandb.Image:
-        return wandb.Image(x.reshape(jax.device_count(), context * resolution, resolution, 3).transpose(1, 0, 2, 3).reshape(context * resolution, jax.device_count() * resolution, 3))
+        return wandb.Image(x.reshape(-1, resolution, 3))
 
     global_step = 0
     for epoch in range(10 ** 9):
@@ -370,10 +367,10 @@ def main(lr: float = 2e-5, beta1: float = 0.9, beta2: float = 0.99, eps: float =
                 print(f"Step {global_step}", datetime.datetime.now())
             i *= device_steps
             batch = {
-                "pixel_values": jnp.transpose(vid, (0,1,2,3,4,5)),#device steps, batch (ordered), context, resolution, resolution, 3
-                "input_ids": jnp.transpose(ids, (0,1,2)),
-                "attention_mask": jnp.transpose(msk, (0,1,2)),
-                "idx": jnp.full((jax.local_device_count(),jax.device_count()),i)}
+                "pixel_values": vid,#device steps, batch (ordered), context, resolution, resolution, 3
+                "input_ids": ids,#device_steps, batch (ordered), input_ids
+                "attention_mask": msk,
+                "idx": jnp.full((jax.local_device_count(),jax.device_count()),i)}#device_steps, batch (ordered), input_ids
             print(f'vid shape AFTER{batch["pixel_values"].shape}, {batch["input_ids"].shape}, {batch["attention_mask"].shape}, {batch["idx"].shape}')
             extra = {}
             pid = f'{jax.process_index() * context * jax.local_device_count()}-{(jax.process_index() + 1) * context * jax.local_device_count() - 1}'
