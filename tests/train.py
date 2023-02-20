e1f10cc8af466b5633496b9821a0e3f6682a309c
Martino Russi <77496684+nepyope@users.noreply.github.com>
2023-02-20 22:37:31 +0100

    fix ppermute, try concat axis 0

--- tests/train.py
+++ tests/train.py
@@ -81,10 +81,10 @@ _original_call = nn.Conv.__call__
 def communicate(x: jax.Array):
 
     def _grad(dy: jax.Array):
-        dy = lax.ppermute(dy, "batch", [((i + 1) % jax.local_device_count(), i) for i in range(jax.local_device_count())])
+        dy = lax.ppermute(dy, "batch", [((i + 1) % jax.device_count(), i) for i in range(jax.device_count())])
         return dy
 
-    x = lax.ppermute(x, "batch", [(i, (i + 1) % jax.local_device_count()) for i in range(jax.local_device_count())])
+    x = lax.ppermute(x, "batch", [(i, (i + 1) % jax.device_count()) for i in range(jax.device_count())])
     return x, _grad
 
 def conv_call(self: nn.Conv, inputs: jax.Array) -> jax.Array:
@@ -364,14 +364,14 @@ def main(lr: float = 2e-5, beta1: float = 0.9, beta2: float = 0.99, eps: float =
                 print(f"Step {global_step}", datetime.datetime.now())
             i *= device_steps
             batch = {
-                "pixel_values": jnp.transpose(vid, (0,1,2,3,4,5)),#this is wrongZZ
-                "input_ids": jnp.transpose(ids, (0,1,2)),
-                "attention_mask": jnp.transpose(msk, (0,1,2)),
-                "idx": jnp.full((jax.local_device_count(),), i, jnp.int64)}
+                "pixel_values": jnp.transpose(vid, (1,0,2,3,4,5)),#this is wrongZZ
+                "input_ids": jnp.transpose(ids, (1,0,2)),
+                "attention_mask": jnp.transpose(msk, (1,0,2)),
+                "idx": jnp.full((jax.device_count(),), i, jnp.int64)}
             
             print(f'vid shape AFTER{batch["pixel_values"].shape}')         
             print(batch['idx'].shape)
-            batch = jax.pmap(lambda x: lax.all_to_all(x, axis_name='i', split_axis=0, concat_axis=1, tiled=True), axis_name='i')(batch)#why the fuck is a 64 here
+            batch = jax.pmap(lambda x: lax.all_to_all(x, axis_name='i', split_axis=0, concat_axis=0, tiled=True), axis_name='i')(batch)
 
             print(f'vid shape all_to_all{batch["pixel_values"].shape}')    
             
