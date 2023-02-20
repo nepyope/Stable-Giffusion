d8d17abf7c467227a557bbc12ecc0d224311b234
Martino Russi <77496684+nepyope@users.noreply.github.com>
2023-02-20 23:57:20 +0100

    merge device steps and jnp.device_count() dim

--- tests/train.py
+++ tests/train.py
@@ -368,10 +368,10 @@ def main(lr: float = 2e-5, beta1: float = 0.9, beta2: float = 0.99, eps: float =
                 print(f"Step {global_step}", datetime.datetime.now())
             i *= device_steps
             batch = {
-                "pixel_values": jnp.transpose(vid, (1,0,2,3,4,5)).reshape(jax.local_device_count(), jax.device_count()//jax.local_device_count(), device_steps, context, resolution, resolution, 3),
-                "input_ids": jnp.transpose(ids, (1,0,2)).reshape(jax.local_device_count(), jax.device_count()//jax.local_device_count(), device_steps, clip_tokens),
-                "attention_mask": jnp.transpose(msk, (1,0,2)).reshape(jax.local_device_count(), jax.device_count()//jax.local_device_count(), device_steps, clip_tokens),
-                "idx": jnp.full((jax.device_count(),), i, jnp.int64).reshape(jax.local_device_count(), jax.device_count()//jax.local_device_count()),}
+                "pixel_values": jnp.transpose(vid, (1,0,2,3,4,5)).reshape(jax.local_device_count(), jax.device_count()*device_steps, context, resolution, resolution, 3),
+                "input_ids": jnp.transpose(ids, (1,0,2)).reshape(jax.local_device_count(), jax.device_count()*device_steps, clip_tokens),
+                "attention_mask": jnp.transpose(msk, (1,0,2)).reshape(jax.local_device_count(), jax.device_count()*device_steps, clip_tokens),
+                "idx": jnp.broadcast_to(jnp.full((jax.device_count(),), i, jnp.int64), (jax.local_device_count(),jax.device_count()))}
             print(f'vid shape AFTER{batch["pixel_values"].shape}, {batch["input_ids"].shape}, {batch["attention_mask"].shape}, {batch["idx"].shape}')
             extra = {}
             pid = f'{jax.process_index() * context * jax.local_device_count()}-{(jax.process_index() + 1) * context * jax.local_device_count() - 1}'
