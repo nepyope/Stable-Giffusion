bee7242b33bed0750cc70afeab3264c32aec2997
Martino Russi <77496684+nepyope@users.noreply.github.com>
2023-02-21 02:12:46 +0100

    enumerate from 1

--- tests/train.py
+++ tests/train.py
@@ -364,7 +364,7 @@ def main(lr: float = 2e-5, beta1: float = 0.9, beta2: float = 0.99, eps: float =
 
     global_step = 0
     for epoch in range(10 ** 9):
-        for i, (vid, ids, msk) in tqdm.tqdm(enumerate(data, 0)):
+        for i, (vid, ids, msk) in tqdm.tqdm(enumerate(data, 1)):
 
             global_step += 1
             if global_step <= 2:
