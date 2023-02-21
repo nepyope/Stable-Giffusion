22509d70421f97afb0066ac39d25470515c60d42
Martino Russi <77496684+nepyope@users.noreply.github.com>
2023-02-21 16:11:31 +0100

    increase resolution (256), decrease context (8)

--- tests/train.py
+++ tests/train.py
@@ -209,7 +209,7 @@ def load(path: str, prototype: Dict[str, jax.Array]):
 
 @app.command()
 def main(lr: float = 2e-4, beta1: float = 0.9, beta2: float = 0.99, eps: float = 1e-16, downloaders: int = 2,
-         resolution: int = 128, fps: int = 8, context: int = 32, workers: int = 64, prefetch: int = 32,
+         resolution: int = 256, fps: int = 8, context: int = 32, workers: int = 64, prefetch: int = 32,
          batch_prefetch: int = 4, base_model: str = "flax_base_model", data_path: str = "./urls",
          sample_interval: int = 2048, parallel_videos: int = 2048, schedule_length: int = 1024, warmup_steps: int = 1024,
          lr_halving_every_n_steps: int = 2 ** 17, clip_tokens: int = 77, save_interval: int = 2048,
