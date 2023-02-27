a762a579c89adf36f7aa48ff4da636b89ba9cb9a
Martino Russi <77496684+nepyope@users.noreply.github.com>
2023-02-27 16:49:11 +0100

    import subprocess

--- tests/data.py
+++ tests/data.py
@@ -16,7 +16,7 @@ from multiprocessing import shared_memory
 from queue import Empty
 from typing import List, Callable, Optional, Tuple, Dict
 import redis 
-
+import subprocess
 import ffmpeg
 import jax
 import numpy as np
