d2d3f67f5e7943dd3b5848fabceb8addacdf3fd1
Martino Russi <77496684+nepyope@users.noreply.github.com>
2023-03-06 14:31:37 +0100

    try precomp

--- tests/train.py
+++ tests/train.py
@@ -425,8 +425,22 @@ def main(lr: float = 5e-7, beta1: float = 0.9, beta2: float = 0.99, eps: float =
 
     p_sample = jax.pmap(sample, "batch")
     p_train_step = jax.pmap(train_step, "batch", donate_argnums=(0, 1))
+    
+    batch = {"pixel_values": jnp.zeros(
+        (jax.local_device_count(), jax.device_count() * video_group, context * resolution * resolution * 3),
+        dtype=jnp.uint8),
+        "input_ids": jnp.zeros((jax.local_device_count(), video_group * jax.device_count(), clip_tokens),
+                               dtype=jnp.int32),
+        "attention_mask": jnp.zeros((jax.local_device_count(), video_group * jax.device_count(), clip_tokens),
+                                    dtype=jnp.int32),
+        "idx": jnp.zeros((jax.local_device_count(),), dtype=jnp.int_)
+    }
+    compile_fn(lambda: p_train_step(jax_utils.replicate(copy.deepcopy(unet_state)), batch), "train step")
     unet_state = jax_utils.replicate(unet_state)
+    compile_fn(lambda: p_sample(unet_state.params, batch), "sampling")
+    del batch
 
+    
     def to_img(x: jax.Array) -> wandb.Image:
         return wandb.Image(x.reshape(-1, resolution, 3))
 
