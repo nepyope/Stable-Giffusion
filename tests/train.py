2fd130e94d3a35ef666c9a73e9bdfd9a39807c5a
nepyope <77496684+nepyope@users.noreply.github.com>
2023-03-08 10:38:05 +0100

    bugfix

--- tests/train.py
+++ tests/train.py
@@ -96,7 +96,7 @@ def wrapper(x, w, *args, **kwargs):
     def _call(a, b):
         def _grad(dy):
             inp = communicate(a)
-            dy, dwgt = jax.jvp(_fn, inp, b)(dy)
+            dy, dwgt = jax.vjp(_fn, inp, b)(dy)
             mid, left, right = jnp.split(dy, 3, -1)
             right, left = rotate(right, left)
             return mid + left + right
